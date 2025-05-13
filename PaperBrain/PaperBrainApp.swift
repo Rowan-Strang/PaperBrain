cfffcab4c0fbe772e22b52bb8072ca1e0487c052
Rowan Strang <rowanstrang@gmail.com>
2025-05-14 10:32:12 +1200

    Initial Commit

---
+++ PaperBrain/PaperBrainApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaperBrainApp.swift
+//  PaperBrain
+//
+//  Created by MacRow on 14/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct PaperBrainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
