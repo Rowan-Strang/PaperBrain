36642a413ab154e337ec185b751a633cfecb1330
Rowan Strang <rowanstrang@gmail.com>
2025-05-14 10:35:59 +1200

    Init Commit

--- PaperBrain/ContentView.swift
+++ PaperBrain/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "scissors.circle.fill")
                 .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+                .foregroundStyle(.black)
+            Text("Welcome to PaperBrain")
         }
         .padding()
     }
