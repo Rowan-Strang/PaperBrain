f37d78f09a98ef5741c74522811926f1acbfffc6
Rowan Strang <rowanstrang@gmail.com>
2025-05-14 10:55:43 +1200

    set up state to store apps choice and win bool

--- PaperBrain/ContentView.swift
+++ PaperBrain/ContentView.swift
@@ -7,13 +7,18 @@
 
 import SwiftUI
 
+let choices = ["Paper", "Scissors", "Rock"]
+
 struct ContentView: View {
+    @State private var appChoice = choices.randomElement()!
+    @State private var shouldWin = Bool.random()
     var body: some View {
         VStack {
             Image(systemName: "scissors.circle.fill")
                 .imageScale(.large)
                 .foregroundStyle(.black)
-            Text("Welcome to PaperBrain")
+            Text("Welcome to PaperBrain, choice is \(appChoice)")
+            Text(shouldWin ? "Win" : "Lose")
         }
         .padding()
     }
