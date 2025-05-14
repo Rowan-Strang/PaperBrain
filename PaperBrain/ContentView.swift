9e6f34df470337e28f5398cdf851ec24edc71ab7
Rowan Strang <rowanstrang@gmail.com>
2025-05-14 12:17:08 +1200

    working code, app rules

--- PaperBrain/ContentView.swift
+++ PaperBrain/ContentView.swift
@@ -7,22 +7,110 @@
 
 import SwiftUI
 
-let choices = ["Paper", "Scissors", "Rock"]
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle.bold())
+            .foregroundStyle(.white)
+    }
+}
+extension View {
+    func titleStyle() -> some View {
+        modifier(Title())
+    }
+}
+
+let choices = ["📄", "✂️", "🪨"]
+let wins: [String: String] = [
+    "📄": "✂️",
+    "✂️": "🪨",
+    "🪨": "📄"
+]
+let loses: [String: String] = [
+    "📄": "🪨",
+    "✂️": "📄",
+    "🪨": "✂️"
+]
 
 struct ContentView: View {
     @State private var appChoice = choices.randomElement()!
+    @State private var playerChoice = ""
     @State private var shouldWin = Bool.random()
+    @State private var round = 1
+    @State private var playerScore = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "scissors.circle.fill")
-                .imageScale(.large)
-                .foregroundStyle(.black)
-            Text("Welcome to PaperBrain, choice is \(appChoice)")
-            Text(shouldWin ? "Win" : "Lose")
+        ZStack{
+            LinearGradient(
+                gradient: Gradient(colors: [shouldWin ? .yellow.opacity(0.8) :.red.opacity(0.7), shouldWin ? .white : .black]),
+                startPoint: .top,
+                endPoint: .bottom
+            )
+            .ignoresSafeArea()
+            VStack {
+
+                VStack{
+                    Text("Score: \(playerScore)/\(round)")
+                        .titleStyle()
+                }
+                Spacer()
+                
+                VStack{
+                    Text("Round \(NumberFormatter.localizedString(from: NSNumber(value: round), number: .spellOut).capitalized)")
+                        .foregroundStyle(.secondary)
+                        .font(.subheadline.weight(.heavy))
+                    
+                    Text(shouldWin ? "Win" : "Lose")
+                        .titleStyle()
+                    Text(appChoice)
+                        .font(.system(size: 150))
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 40)
+                .background(.ultraThinMaterial)
+                .clipShape(.rect(cornerRadius: 20))
+                Spacer()
+                VStack{
+                    Button("Flip"){
+                        shouldWin.toggle()
+                        appChoice = choices.randomElement()!
+                    }
+                    Text("Your Choice: \(playerChoice)")
+                        .foregroundStyle(.secondary)
+                        .font(.subheadline.weight(.heavy))
+                    HStack{
+                        ForEach (choices.shuffled(), id: \.self) { choice in
+                            Button(choice){
+                                choiceTapped(choice)
+                            }
+                            .font(.system(size: 50))
+                        }
+                        .padding(20)
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 40)
+                .background(.ultraThinMaterial)
+                .clipShape(.rect(cornerRadius: 20))
+                Spacer()
+                Spacer()
+            }
+            .padding()
         }
-        .padding()
+    }
+    func choiceTapped (_ choice: String){
+        playerChoice = choice
+        round += 1
+        if appChoice == (shouldWin ? loses[choice] : wins[choice]) {
+            playerScore += 1
+        } else {
+            playerScore -= 1
+        }
+        appChoice = choices.randomElement()!
+        shouldWin.toggle()
     }
 }
+
 
 #Preview {
     ContentView()
